bb487e3887b249ce8fbdd2782faae744c2cb410f
Alejandrolt <alejandrolt54@gmail.com>
2024-04-11 11:06:23 -0500

    taskreact3-2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 const ButtonLoad = styled.button`
-  background-color: #77dd77;
+  background-color: #0e78a3;
   border: none;
   border-radius: 5px;
   color: #fff;
